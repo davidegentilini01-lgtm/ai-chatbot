4a8c7d945209f74e3dfda40edc9df9844763fe54
davidegentilini01-lgtm <davide.gentilini01@gmail.com>
2026-05-04 20:25:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -26,4 +26,7 @@ app.post("/chat", async (req, res) => {
   res.json({ reply: data.choices?.[0]?.message?.content || "Errore" });
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
